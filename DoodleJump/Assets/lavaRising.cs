540ba8d562f4a49d027eafb699145c8ceae9a6ec
amusipatla <amusipatla@gmail.com>
2019-03-02 15:43:54 -0500

    lava block rises

---
+++ DoodleJump/Assets/lavaRising.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lavaRising : MonoBehaviour
+{
+    private Vector2 currentVelocity;
+    public float smoothSpeed = 3.0f;
+
+    void Update()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        Vector2 direction = new Vector2(0, smoothSpeed);
+        rb.MovePosition(rb.position + direction * Time.deltaTime);
+    }
+
+}
