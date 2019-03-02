4524e63850cd686cd222f6aa59ad5707ae4d4a8e
amusipatla <amusipatla@gmail.com>
2019-03-02 16:23:01 -0500

    lava triggers when doodler hits it

--- DoodleJump/Assets/lavaRising.cs
+++ DoodleJump/Assets/lavaRising.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class lavaRising : MonoBehaviour
 {
     private Vector2 currentVelocity;
     public float smoothSpeed = 3.0f;
+    Rigidbody2D rb;
 
-    void Update()
+    void Start()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
 
+
+    private void Update()
+    {
         Vector2 direction = new Vector2(0, smoothSpeed);
         rb.MovePosition(rb.position + direction * Time.deltaTime);
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("DED");
+    }
 }
